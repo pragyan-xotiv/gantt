f46e300870a098bba83cb627f74118f1c41f6249
Pragyan Deep Gogoi <pragyan.gagoi@xotiv.com>
2024-07-24 15:58:06 +0530

    adjusted chart height

--- src/index.js
+++ src/index.js
@@ -330,7 +330,7 @@ export default class Gantt {
         });
 
         $.attr(this.$svg, {
-            height: grid_height + this.options.padding + 100,
+            height: grid_height + this.options.padding - 24,
             width: '100%',
         });
     }
